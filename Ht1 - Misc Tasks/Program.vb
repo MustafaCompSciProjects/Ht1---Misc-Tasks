710548a8d0c640551c26ca278eae02b6b08987df
mustafa <25029104@sjd.ac.uk>
2025-10-28 10:21:49 -0700

    Added FactorialFinder and SpeedTracker
    Thief WIP

--- Ht1 - Misc Tasks/Program.vb
+++ Ht1 - Misc Tasks/Program.vb
@@ -1,4 +1,6 @@
 Imports System
+Imports System.IO
+Imports System.Numerics
 
 Module Program
     Function GetNumericalInput(Optional ByVal CanBeDecimal As Boolean = False, Optional ByVal minVal As Integer = -2147483648, Optional ByVal maxVal As Integer = 2147483647)
@@ -17,7 +19,12 @@ Module Program
                     If CanBeDecimal Then
                         Return UserInput
                     Else
-                        Return Int(UserInput)
+                        If UserInput = Int(UserInput) Then
+                            Return UserInput
+                        Else
+                            Console.WriteLine("Please enter a valid number")
+                        End If
+
                     End If
 
 
@@ -36,9 +43,9 @@ Module Program
     Function SelectProgram()
         Console.WriteLine("
 Which program would you like to use
-1.
-2.
-3.
+1. Factorial Finder
+2. Speed Tracker
+3. Thief
 4.
 5.
 6.
@@ -50,33 +57,159 @@ Which program would you like to use
     End Function
 
     Sub Main(args As String())
-        Dim ProgramSelected As Integer = SelectProgram()
-
-        If ProgramSelected = 1 Then
-            Console.WriteLine("1")
-        ElseIf ProgramSelected = 2 Then
-            Console.WriteLine("2")
-        ElseIf ProgramSelected = 3 Then
-            Console.WriteLine("3")
-        ElseIf ProgramSelected = 4 Then
-            Console.WriteLine("4")
-        ElseIf ProgramSelected = 5 Then
-            Console.WriteLine("5")
-        ElseIf ProgramSelected = 6 Then
-            Console.WriteLine("6")
-        ElseIf ProgramSelected = 7 Then
-            Console.WriteLine("7")
-        ElseIf ProgramSelected = 8 Then
-            Console.WriteLine("8")
-        ElseIf ProgramSelected = 9 Then
-            Console.WriteLine("Goodbye, World!")
-            Environment.Exit(0)
-        End If
+        Dim ProgramSelected As Integer
 
 
+        While True
+            ProgramSelected = SelectProgram()
+            If ProgramSelected = 1 Then
+                Console.Clear()
+                FactorialFinder()
+            ElseIf ProgramSelected = 2 Then
+                Console.Clear()
+                SpeedTracker()
+            ElseIf ProgramSelected = 3 Then
+                Console.Clear()
+                Thief()
+            ElseIf ProgramSelected = 4 Then
+                Console.WriteLine("4")
+            ElseIf ProgramSelected = 5 Then
+                Console.WriteLine("5")
+            ElseIf ProgramSelected = 6 Then
+                Console.WriteLine("6")
+            ElseIf ProgramSelected = 7 Then
+                Console.WriteLine("7")
+            ElseIf ProgramSelected = 8 Then
+                Console.WriteLine("8")
+            ElseIf ProgramSelected = 9 Then
+                Console.WriteLine("Goodbye, World!")
+                Environment.Exit(0)
+            End If
+
+        End While
 
 
     End Sub
 
+    Sub FactorialFinder()
+        Console.WriteLine("Please enter the number you want to find the factorial of")
+
+        Dim FactorialNum As BigInteger = BigInteger.Parse(GetNumericalInput())
+
+        For i As Integer = 1 To FactorialNum - 1
+            FactorialNum *= i
+        Next
+
+        If FactorialNum = 0 Then
+            FactorialNum = 1
+        End If
+        Console.WriteLine(FactorialNum)
+        Console.ReadLine()
+    End Sub
+    Function GetTimeInput()
+        Dim UserInput As String
+        While True
+            UserInput = Console.ReadLine
+            Try
+                Return TimeOnly.Parse(UserInput)
+            Catch ex As Exception
+                Console.WriteLine("Please enter a valid time in format HH:MM:SS")
+            End Try
+        End While
+    End Function
+    Function GetLicensePlate()
+        'https://assets.publishing.service.gov.uk/media/6694e379fc8e12ac3edafc60/inf104-vehicle-registration-numbers-and-number-plates.pdf
+        Dim Valid As Boolean = False
+        Dim UserInput As String
+        Dim ValidLetters As String = "ABCDEFGHIJKLMNPRSTUVWXYZ" 'All letters except O, Q
+        While Not Valid
+            UserInput = Console.ReadLine
+            UserInput = UserInput.Replace(" ", "")
+
+            If Len(UserInput) = 7 Then 'Verify Length
+                Valid = True
+                For Each i As Char In UserInput
+                    If (ValidLetters.Contains(i) And Char.IsUpper(i)) Or IsNumeric(i) Then
+
+                    Else
+                        Valid = False
+                    End If
+                Next
+            Else
+
+            End If
+            If Not Valid Then
+                Console.WriteLine("Please enter a valid UK License plate")
+            Else
+                Return UserInput
+            End If
+        End While
+
+    End Function
+    Function GetRandomPlateNumber()
+        Dim Rnd As New Random
+        Dim Alphabet As String = "ABCDEFGHIJKLMNPQRSTUVWXYZ"
+        Dim PlateNumber As String = ""
+
+        'Generate plate number using current standard of AB12 CDE
+        PlateNumber = PlateNumber & Alphabet(Rnd.Next(0, 25))
+        PlateNumber = PlateNumber & Alphabet(Rnd.Next(0, 25))
+        PlateNumber = PlateNumber & Rnd.Next(0, 9)
+        PlateNumber = PlateNumber & Rnd.Next(0, 9)
+        PlateNumber = PlateNumber & " "
+        PlateNumber = PlateNumber & Alphabet(Rnd.Next(0, 25))
+        PlateNumber = PlateNumber & Alphabet(Rnd.Next(0, 25))
+        PlateNumber = PlateNumber & Alphabet(Rnd.Next(0, 25))
+        Return PlateNumber
+
+    End Function
+    Sub SpeedTracker()
+        'Add a few random plates to the list of ticketed vehicles
+        Dim rnd As New Random
+        For i As Integer = 0 To rnd.Next(4, 6)
+            File.AppendAllText("TicketsToGive.txt", GetRandomPlateNumber() & vbNewLine)
+        Next
+
+        'Get the times from both cameras
+        Console.WriteLine("Please enter the time at speed camera 1")
+        Dim Time1 As TimeOnly = GetTimeInput()
+
+        Console.WriteLine("Please enter the time at speed camera 2")
+        Dim Time2 As TimeOnly = GetTimeInput()
+
+        Console.WriteLine("Please enter the license plate of the vehicle")
+        Dim PlateNumber As String = GetLicensePlate()
+
+
+        'Calculate the time between the cameras
+        Dim TimeDelta As TimeSpan
+        TimeDelta = Time2.ToTimeSpan - Time1.ToTimeSpan
+
+        'Convert the time delta to hours
+        Dim TimeInHours As Double
+        TimeInHours += TimeDelta.Hours
+        TimeInHours += TimeDelta.Minutes / 60
+        TimeInHours += TimeDelta.Seconds / 60 / 60
+
+        'Calculate speed
+        Dim MPH As Double = 1 / TimeInHours
+
+        'If speed is over 70mph, add to the list of tickets to give
+        If MPH > 70 Then
+            File.AppendAllText("TicketsToGive.txt", PlateNumber & vbNewLine)
+            Console.WriteLine(Double.Round(MPH, 2) & "Mph, This person has been ticketed")
+        Else
+            Console.WriteLine(Double.Round(MPH, 2) & "Mph")
+        End If
+        Console.ReadLine()
+    End Sub
+    Sub Thief()
+        Console.WriteLine("Please enter your 4 known digits")
+        Dim Digits As String = GetNumericalInput(False, 1000, 9999)
+        Dim AllCombos As New List(Of Integer)
+        Dim UniqueCombos As New List(Of Integer)
+
+
+    End Sub
 
 End Module
